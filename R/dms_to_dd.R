3f90aad5b73309c15107f51e34e9f1194f9a4363
maxbre <maxbre67@gmail.com>
2021-10-29 09:26:49 +0000

    add function dms_to_dd

---
+++ R/dms_to_dd.R
@@ -0,0 +1,29 @@
+#' Degrees minutes seconds to decimal degrees
+#'
+#' @param s a vector of strings in the form "degrees minutes seconds" separated by a blank space
+#'
+#' @return a numeric vector in decimal degrees
+#' @export
+#'
+#' @examples
+#' myangle<- '45  12    3.01'
+#' dms_to_dd(myangle)
+
+dms_to_dd <- function(s) {
+
+  x <- do.call(rbind, strsplit(s, split='\\s+'))
+  #x <- do.call(rbind, strsplit(se, split="[[:space:]]+"))
+
+  x <- apply(x, 1, function(y) {
+
+    y <- as.numeric(y)
+
+    y[1] + y[2]/60 + y[3]/3600
+
+    })
+
+  x
+}
+
+
+
