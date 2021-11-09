681bf729c5df20f885fd1a313aaa2fe8f7edb25a
maxbre <maxbre67@gmail.com>
2021-11-09 10:36:16 +0000

    small

--- R/stat_get_mode.R
+++ R/stat_get_mode.R
@@ -1,15 +1,16 @@
-#' Get the mode
+#' stat_get_mode
+#' 
+#' get the mode of a numeric vector
 #'
 #' @param v a numeric vector
 #'
 #' @return get the mode (the most frequent value)
 #' @export
-#'
 #' @examples
 #' v<- c(NA, 19, 4, 5, 7, 29, 19, 29, 13, 25, 19, 19)
-#' mode(v)
+#' get_mode(v)
 
-get_mode <- function(v) {
+stat_get_mode <- function(v) {
 
   # this is dealing with NA
   uniqv <- stats::na.omit(unique(v))
