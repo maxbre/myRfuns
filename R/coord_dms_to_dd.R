9ecd3ec706c05298996c321d655bfcdad613b21f
maxbre <maxbre67@gmail.com>
2021-11-09 10:34:33 +0000

    small

--- R/coord_dms_to_dd.R
+++ R/coord_dms_to_dd.R
@@ -1,15 +1,15 @@
+#' coord_dms_to_dd
+#' 
 #' Degrees minutes seconds to decimal degrees
 #'
 #' @param s a vector of strings in the form "degrees minutes seconds" separated by a blank space
-#'
 #' @return a numeric vector in decimal degrees
 #' @export
-#'
 #' @examples
 #' myangle<- "45  12    3.01"
-#' dms_to_dd(myangle)
+#' coord_dms_to_dd(myangle)
 
-dms_to_dd <- function(s) {
+coord_dms_to_dd <- function(s) {
 
   x <- do.call(rbind, strsplit(s, split='\\s+'))
   #x <- do.call(rbind, strsplit(se, split="[[:space:]]+"))
